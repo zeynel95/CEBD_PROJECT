9aa5a9ea4341d1ad47d909dba49658b0ba36a8fc
RonaldDomi <ronalddomi4@gmail.com>
2022-12-01 17:10:53 +0100

    Creation Triggers

--- data/v1_deleteDB.sql
+++ data/v1_deleteDB.sql
@@ -8,4 +8,11 @@ DROP VIEW IF EXISTS LesAgesSportifs;
 DROP VIEW IF EXISTS LesNbsEquipiers;
 DROP VIEW IF EXISTS ageMoyEq;
 DROP VIEW IF EXISTS classementPays;
+DROP TRIGGER IF EXISTS TriggerInscriptionNumInsert;
+DROP TRIGGER IF EXISTS TriggerInscriptionNumUpdate;
+DROP TRIGGER IF EXISTS TriggerInscriptionNumInsertPasse;
+DROP TRIGGER IF EXISTS TriggerInscriptionNumUpdatePasse;
+DROP TRIGGER IF EXISTS TriggerInscriptionNumDeletePasse;
+DROP TRIGGER IF EXISTS TriggerEquipePays;
+DROP TRIGGER IF EXISTS TriggerEquipePaysUpdate;
 -- TODO 3.3 : pensez à détruire vos triggers !
